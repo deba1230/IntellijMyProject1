a118bd7b19083eb9f38d65f82731f2c40446f0fc
deba1230 <rdeba.ctc@gmail.com>
2024-05-04 12:10:30 +0530

    Updated through IntellijIDE

--- src/test/java/MyPackage1/ArraySize.java
+++ src/test/java/MyPackage1/ArraySize.java
@@ -2,11 +2,11 @@ package MyPackage1;
 
 public class ArraySize
 {
-    public static void main(String[] args)
-    {
-        int[] a = {8000,4000,3000,40,50,60,750,100};
-        int size=a.length;
-        System.out.println("The siz of array:"+size);
+    public static void main(String[] args) {
+        int[] a = {8000, 4000, 3000, 40, 50, 60, 750, 100};
+        int size = a.length;
+        System.out.println("The siz of array:" + size);
         System.out.println("Hai......");
+        System.out.println("updated in prg");
     }
 }
